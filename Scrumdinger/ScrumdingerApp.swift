e0fcf2cfaa57f63d46f4cbffb4ffad0c0f0451ca
Stephen_Opoku_Damoah <opokudamoahstephen@gmail.com>
2021-02-28 11:35:20 +0000

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Stephen Opoku Damoah on 28/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
