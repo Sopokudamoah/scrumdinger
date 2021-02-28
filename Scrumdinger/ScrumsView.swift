64098fd6d855efc384ad53bcdf0ee49f63be44f4
Stephen_Opoku_Damoah <opokudamoahstephen@gmail.com>
2021-02-28 14:25:22 +0000

    Added navigation heirachy

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -11,15 +11,25 @@ struct ScrumsView: View {
     let scrums : [DailyScrum]
     var body: some View {
         List {
-            ForEach(scrums) { scrum in CardView(scrum: scrum)
-                    .listRowBackground(scrum.color)
+            ForEach(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                    
+                }.listRowBackground(scrum.color)
             }
         }
+        .navigationTitle("Daily Scrums")
+        .navigationBarItems(trailing: Button(action: {}) {
+                    Image(systemName: "plus")
+                })
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.data)
+        NavigationView{
+            ScrumsView(scrums: DailyScrum.data)
+        }
+        
     }
 }
