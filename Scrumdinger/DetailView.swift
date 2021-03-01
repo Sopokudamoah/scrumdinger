41feb55db30c303ed3c603a8545c28bef44aefe8
Stephen_Opoku_Damoah <opokudamoahstephen@gmail.com>
2021-03-01 00:49:57 +0000

    worked on meeting screen state

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -15,7 +15,7 @@ struct DetailView: View {
         List{
             Section(header: Text("Meeting Info")) {
                 NavigationLink(
-                    destination: MeetingView()){
+                    destination: MeetingView(scrum: $scrum)){
                     Label("Start Meeting", systemImage: "timer")
                         .accessibilityLabel(Text("Start meeting"))
                         .font(.headline)
